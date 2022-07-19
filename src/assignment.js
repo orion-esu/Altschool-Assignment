d4d25f9cc427c9fb89ee9082a35d11a99451c358
Esu Gabriel <orionesu.dev@gmail.com>
2022-07-19 21:43:06 +0100

    fixed error

--- src/assignment.js
+++ src/assignment.js
@@ -3,27 +3,27 @@
 const assignment = {};
 // ========================
 
-
 /**
  * Challenge - 1
- * 
+ *
  * Calculate and return the sum of the numbers in an array.
  * If you did Challenge - 1, remove the comment in the line just after this function
- * 
+ *
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.reduce((prev, next) => prev + next);
+    const numSum = arrayOfNumbers.reduce((prev, next) => prev + next);
+    return numSum;
 }
 assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
- * 
+ *
  * Count all even numbers within an array of numbers.
  * If you did Challenge - 2, remove the comment in the line just after this function
- * 
+ *
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
@@ -34,26 +34,25 @@ function countEvenNumbers(arrayOfNumbers) {
 
 /**
  * Challenge - 3
- * 
+ *
  * Given an array of numbers representing temperatures in Celsius,
  * convert each number to Fahrenheit and return a new array containing the converted
  * temperatures in Fahrenheit. Decimal figures in the converted values in Fahrenheit should be removed.
  * E.g 51.21 should just be 51 (hint: Math.trunc(...) function)
  * If you did Challenge - 3, remove the comment in the line just after this function
- * 
+ *
  * See https://www.thoughtco.com/celcius-to-farenheit-formula-609227 for the conversion formula
- * 
+ *
  * @param {Array} arrayOfNumbers the array containing temperatures in Celsius to be converted
  * @returns Array the converted temperatures in Fahrenheit
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers.map(el => Math.trunc(((el * 1.8) + 32)));
+    const celToFar = arrayOfNumbers.map((el) => Math.trunc(el * 1.8 + 32));
+    return celToFar;
 }
 assignment.celsiusToFahrenheit = celsiusToFahrenheit;
-
 
 // ========================
 // DO NOT EDIT THIS BLOCK
 module.exports = assignment;
 // ========================
-
